f6f73fda01394ddc8bdeba18176e0387878211de
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2019-01-26 14:35:47 +0100

    basic endgame

--- Assets/Scripts/Home/CharacterSelection.cs
+++ Assets/Scripts/Home/CharacterSelection.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class CharacterSelection : MonoBehaviour
 {
@@ -9,12 +10,22 @@ public class CharacterSelection : MonoBehaviour
     [SerializeField] private Text Description;
     [SerializeField] private Text Comment;
 
+    [SerializeField] private List<GameObject> DeactivateOnEndGame;
+    [SerializeField] private List<GameObject> ActivateOnEndGame;
+
     private void Start()
     {
-        if (DataManager.Instance.ChosenPerson.Status != PersonStatus.Available)
-            DataManager.Instance.ChooseNextAvailablePerson();
+        if (DataManager.Instance.People.TrueForAll(person => person.Status != PersonStatus.Available))
+        {
+            EndGame();
+        }
+        else
+        {
+            if (DataManager.Instance.ChosenPerson.Status != PersonStatus.Available)
+                DataManager.Instance.ChooseNextAvailablePerson();
 
-        Refresh();
+            Refresh();
+        }
     }
 
     public void Next()
@@ -43,5 +54,14 @@ public class CharacterSelection : MonoBehaviour
         Name.text = DataManager.Instance.ChosenPerson.Name;
         Description.text = DataManager.Instance.ChosenPerson.Bio;
         Comment.text = DataManager.Instance.ChosenPerson.FurnitureComment;
+    }
+
+    private void EndGame()
+    {
+        foreach (var go in DeactivateOnEndGame)
+            go.SetActive(false);
+
+        foreach (var go in ActivateOnEndGame)
+            go.SetActive(true);
     }
 }

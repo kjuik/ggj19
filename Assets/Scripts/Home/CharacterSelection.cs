b9fea57643ce4cb960ebaffbec59b45e1d25d0b6
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2019-01-26 02:14:14 +0100

    working character selection and switching to date scene

---
+++ Assets/Scripts/Home/CharacterSelection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField] private Image Photo;
+    [SerializeField] private Text Name;
+    [SerializeField] private Text Description;
+    [SerializeField] private Text Comment;
+
+    private void Start() => Refresh();
+
+    public void Next()
+    {
+        DataManager.Instance.ChooseNextPerson();
+        Refresh();
+    }
+
+    public void Date()
+    {
+        SceneManager.LoadScene("Date");
+    }
+
+    private void Refresh()
+    {
+        Photo.sprite = DataManager.Instance.ChosenPerson.MetaData.Photo;
+        Name.text = DataManager.Instance.ChosenPerson.Name;
+        Description.text = DataManager.Instance.ChosenPerson.Bio;
+        Comment.text = DataManager.Instance.ChosenPerson.FurnitureComment;
+    }
+}

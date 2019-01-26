5e41e9d91e126556a781d140901bc7003b01f460
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2019-01-26 15:07:44 +0100

    basic text scrolling

---
+++ Assets/Scripts/TextScrolling.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScrolling : MonoBehaviour
+{
+    [SerializeField] float secondsPerLetter = 0.025f;
+
+    Text text;
+
+    string fullText;
+    string currentText;
+    int nextLetterIndex;
+
+    void Awake() =>
+        text = GetComponent<Text>();
+        
+    public void Scroll()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScrollCoroutine());
+    }
+
+    IEnumerator ScrollCoroutine()
+    {
+        fullText = text.text;
+        string currentText = "";
+        nextLetterIndex = 0;
+
+        text.text = currentText;
+
+        while (currentText != fullText)
+        {
+            yield return new WaitForSeconds(secondsPerLetter);
+
+            currentText += fullText[nextLetterIndex];
+            nextLetterIndex++;
+
+            text.text = currentText;
+        }
+    }
+}
